9fe08df5ccf2498e4d7cb6decd57287d5295cacb
Richard Martin <richardgeorgemartin@gmail.com>
2014-06-06 11:01:16 -0400

    initial crm.rb commit

---
+++ crm.rb
@@ -0,0 +1,90 @@
+# crm file within richard's crm application
+
+require_relative "contact"
+require_relative "rolodex"
+
+# define the class "CRM"
+
+class CRM
+
+	# need attribute declaration for "name"
+	# determine how to handle "user_selected" variable
+
+
+	def initialize(name)
+		# some code creating the instance variable ... i think
+		# @name = name
+	end
+		
+	def print_main_menu
+	  puts "[1] Add a new contact"
+	  puts "[2] Modify an existing contact"
+	  puts "[3] Delete a contact"
+	  puts "[4] Display all the contacts"
+	  puts "[5] Display an attribute"
+	  puts "[6] Exit"
+	  puts "Enter a number: "
+	end
+
+	def main_menu
+	  print_main_menu
+	  user_selected = gets.to_i
+	  call_option(user_selected)
+	end
+
+	def call_option(user_selected)
+  	  # add_new_contact if user_selected == 1
+  	  # modify_existing_contact if user_selected == 2
+  	  # delete_contact if user_selected == 3
+     #  display_all_contacts if user_selected == 4
+     #  display_attribute if user_selected == 5
+     #  exit_crm if user_selected == 6
+
+      case user_selected
+      when 1 then add_new_contact
+      when 2 then modify_existing_contact
+      when 3 then delete_contact
+      when 4 then display_all_contacts
+      when 5 then display_attribute
+      when 6
+      	puts "Goodbye"
+      	return
+      else
+      	puts "Invalid option. Please try again"
+      	main_menu
+      end							
+	end
+
+	def add_new_contact
+	  print "Enter First Name: "
+  	  first_name = gets.chomp
+      print "Enter Last Name: "
+      last_name = gets.chomp
+      print "Enter Email Address: "
+      email = gets.chomp
+      print "Enter a Note: "
+      note = gets.chomp
+      contact = Contact.new(first_name, last_name, email, note)
+	end
+	
+	def modify_existing_contact
+		# some code
+	end
+
+	def delete_contact
+		# some code
+	end
+
+	def display_all_contacts
+		# some code
+	end
+
+	def display_attribute
+		# some code
+	end
+
+	def exit_crm
+		# some code
+	end
+			
+end
